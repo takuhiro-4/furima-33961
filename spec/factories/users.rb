56d529896f06eb439038278a12ba3f1c3b93fc97
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-02 16:34:37 +0900

    テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    kjname                {Faker::Name.initials(number: 2)}
+    namekj                {Faker::Name.initials(number: 2)}
+    knname                {Faker::Name.initials(number: 2)}
+    namekn                {Faker::Name.initials(number: 2)}
+    dob                   {Faker::Date.birthday(min_age: 18, max_age: 65)}
+  end
+end
