d661e98cf1f37a084c411ce9c84f75ad8e06aa81
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-03 11:15:56 +0900

    ユーザー管理機能に不要なコードの消去

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,19 +3,4 @@ class ItemsController < ApplicationController
     @users = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-  end
-
-  def destroy
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:nickname, :kjname, :namekj, :knname, :namekn, :dob)
-  end
 end
