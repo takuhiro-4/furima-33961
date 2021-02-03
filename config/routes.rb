c1e4bae8c7ecad293208bf6f8bf3c4b8d6541de1
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-03 14:53:23 +0900

    ルーティングの修正
    差分を無くす為に不要な記述を消去

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  get 'itmes/index'
-  resources :items, only: [:inedx, :new, :create, :destroy]
+  resources :items, only: :inedx
 end
