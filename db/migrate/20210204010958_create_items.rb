9d776c0f34f4b03c64a266d47f162c4aded51a36
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-05 17:26:04 +0900

    マイグレーションファイルの記述

---
+++ db/migrate/20210204010958_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string     :name,        null: false
+      t.text       :description, null: false
+      t.integer    :kategory_id, null: false
+      t.integer    :state_id,    null: false
+      t.integer    :burden_id,   null: false
+      t.integer    :area_id,     null: false
+      t.integer    :sipping_id,  null: false
+      t.integer    :price,       null: false
+      t.references :user,        null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
