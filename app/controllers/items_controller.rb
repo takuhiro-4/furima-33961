414d9a3554a03fbe41f465989e51a5f5dc95e6bd
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-08 14:52:28 +0900

    URLを直接入力してトップページに戻る

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :abc, only: :edit
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -41,4 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :kategory_id, :state_id, :burden_id, :area_id, :sipping_id,
                                  :price).merge(user_id: current_user.id)
   end
+
+  def abc
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id 
+      redirect_to action: :index
+    end
+  end
+
 end
