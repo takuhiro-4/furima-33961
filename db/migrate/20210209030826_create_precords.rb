bfaec41a8bf2be759936c295574582183a3da418
takuhiro-4 <s.takuhiro@icloud.com>
2021-02-09 18:55:21 +0900

    precordのモデル・マイグレーションファイル

---
+++ db/migrate/20210209030826_create_precords.rb
@@ -0,0 +1,10 @@
+class CreatePrecords < ActiveRecord::Migration[6.0]
+  def change
+    create_table :precords do |t|
+      t.references :user,  foreign_key: true
+      t.references :item,  foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
